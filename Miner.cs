e2d8f530b862af0d3790abcb59ec25cb7f2821fe
ShadowGameDeveloper <67132655+ShadowGameDeveloper@users.noreply.github.com>
2020-12-08 21:52:14 +0900

    Intial

---
+++ Miner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Miner : BaseGameEntity
+{
+    State currentState;
+
+    int goldPerson;
+    int goldInBank;
+    int thrist;
+    int fatigue;
+
+    public void ChangeState(State newState)
+    {
+        if(currentState != null)
+        {
+            currentState.Exit(this);
+        }
+        currentState = newState;
+        currentState.Enter(this);
+    }
+
+    protected void FixedUpdate()
+    {
+        thrist++;
+        if(currentState != null)
+        {
+            currentState.Execute(this);
+        }
+    }
+}
